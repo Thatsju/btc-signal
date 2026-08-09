ed43d27f3c2aa3b4074bd04921d79ace560ec92a
Thatsju <juliealepee@live.fr>
2026-08-09 19:20:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -326,9 +326,7 @@ console.log(
     "DEBUG DETAIL COMPORTEMENT",
     {
         score: score,
-        prix: currentPrice,
-        variation30: variation30,
-        variation90: variation90
+        prix: currentPrice
     }
 );
 
